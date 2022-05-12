9a68588aa0fec938d921a079a70a1b6a3b742195
Htocki <pasztownia@gmail.com>
2022-05-12 20:38:40 +0300

    Усовершенствование игрового цикла. Сохранение наработок по вращению фигуры.

---
+++ compile.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+fpc -Fu"./module/matrix/;./module/field/;./module/figure/;./module/console/;./module/state/;./module/key/;./module/keyboard/;./module/model/;./module/target/;module/user/" tetris.pas
+
