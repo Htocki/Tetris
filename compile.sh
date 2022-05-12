76710751a1ebbe54b6d124f82c1ce8a66763d600
Htocki <pasztownia@gmail.com>
2022-05-12 20:42:15 +0300

    Revert "Усовершенствование игрового цикла. Сохранение наработок по вращению фигуры."
    This reverts commit 9a68588aa0fec938d921a079a70a1b6a3b742195.

--- compile.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-fpc -Fu"./module/matrix/;./module/field/;./module/figure/;./module/console/;./module/state/;./module/key/;./module/keyboard/;./module/model/;./module/target/;module/user/" tetris.pas
-
