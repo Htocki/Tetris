e37b2dd07d8bb79474443beb0b0857eda63d876e
Htocki <pasztownia@gmail.com>
2022-10-12 15:10:00 +0300

    Переделка скрипта удаления.

--- remove.sh
+++ remove.sh
@@ -1,130 +1,15 @@
 #!/bin/bash
 
-echo -e "\nRemove all files other than source code files."
-
-rm tetris
-rm tetris.o
-
-rm **/console/uconsole.o
+# Удаляет файлы модулей, которые почему-то иногда не перекомпилируются.
 rm **/console/uconsole.ppu
-
-rm **/element/uelement.o
 rm **/element/uelement.ppu
-
-rm **/field/ufield.o
 rm **/field/ufield.ppu
-
-rm **/figure/ufigure.o
 rm **/figure/ufigure.ppu
-rm **/figure/**/add_i_down
-rm **/figure/**/add_i_down.o
-rm **/figure/**/add_i_left
-rm **/figure/**/add_i_left.o
-rm **/figure/**/add_i_right
-rm **/figure/**/add_i_right.o
-rm **/figure/**/add_i_top
-rm **/figure/**/add_i_top.o
-rm **/figure/**/add_j_down
-rm **/figure/**/add_j_down.o
-rm **/figure/**/add_j_left
-rm **/figure/**/add_j_left.o
-rm **/figure/**/add_j_right
-rm **/figure/**/add_j_right.o
-rm **/figure/**/add_j_top
-rm **/figure/**/add_j_top.o
-rm **/figure/**/add_l_down
-rm **/figure/**/add_l_down.o
-rm **/figure/**/add_l_left
-rm **/figure/**/add_l_left.o
-rm **/figure/**/add_l_right
-rm **/figure/**/add_l_right.o
-rm **/figure/**/add_l_top
-rm **/figure/**/add_l_top.o
-rm **/figure/**/add_o_down
-rm **/figure/**/add_o_down.o
-rm **/figure/**/add_o_left
-rm **/figure/**/add_o_left.o
-rm **/figure/**/add_o_right
-rm **/figure/**/add_o_right.o
-rm **/figure/**/add_o_top
-rm **/figure/**/add_o_top.o
-rm **/figure/**/add_s_down
-rm **/figure/**/add_s_down.o
-rm **/figure/**/add_s_left
-rm **/figure/**/add_s_left.o
-rm **/figure/**/add_s_right
-rm **/figure/**/add_s_right.o
-rm **/figure/**/add_s_top
-rm **/figure/**/add_s_top.o
-rm **/figure/**/add_t_down
-rm **/figure/**/add_t_down.o
-rm **/figure/**/add_t_left
-rm **/figure/**/add_t_left.o
-rm **/figure/**/add_t_right
-rm **/figure/**/add_t_right.o
-rm **/figure/**/add_t_top
-rm **/figure/**/add_t_top.o
-rm **/figure/**/add_z_down
-rm **/figure/**/add_z_down.o
-rm **/figure/**/add_z_left
-rm **/figure/**/add_z_left.o
-rm **/figure/**/add_z_right
-rm **/figure/**/add_z_right.o
-rm **/figure/**/add_z_top
-rm **/figure/**/add_z_top.o
-rm **/figure/**/deactivate
-rm **/figure/**/deactivate.o
-rm **/figure/**/delete
-rm **/figure/**/delete.o
-rm **/figure/**/initialize
-rm **/figure/**/initialize.o
-rm **/figure/**/move_down
-rm **/figure/**/move_down.o
-rm **/figure/**/move_left
-rm **/figure/**/move_left.o
-rm **/figure/**/move_right
-rm **/figure/**/move_right.o
-rm **/figure/**/next_orientation
-rm **/figure/**/next_orientation.o
-rm **/figure/**/read_form
-rm **/figure/**/read_form.o
-rm **/figure/**/read_orientation
-rm **/figure/**/read_orientation.o
-rm **/figure/**/read_position
-rm **/figure/**/read_position.o
-rm **/figure/**/rotate_right
-rm **/figure/**/rotate_right.o
-rm **/figure/**/write_form
-rm **/figure/**/write_form.o
-rm **/figure/**/write_orientation
-rm **/figure/**/write_orientation.o
-rm **/figure/**/write_position
-rm **/figure/**/write_position.o
-
-rm **/key/ukey.o
 rm **/key/ukey.ppu
-
-rm **/keyboard/ukeyboard.o
 rm **/keyboard/ukeyboard.ppu
-
-rm **/matrix/umatrix.o
 rm **/matrix/umatrix.ppu
-rm **/matrix/**/clear
-rm **/matrix/**/copy
-rm **/matrix/**/fill
-rm **/matrix/**/is_equal
-
-rm **/model/umodel.o
 rm **/model/umodel.ppu
-
-rm **/state/ustate.o
 rm **/state/ustate.ppu
-
-rm **/target/utarget.o
 rm **/target/utarget.ppu
-
-rm **/test/utest.o
 rm **/test/utest.ppu
-
-rm **/user/uuser.o
 rm **/user/uuser.ppu
