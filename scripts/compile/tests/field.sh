9f7c8483d25c0dc84f35593dc0f3c45917636ff7
Htocki <pasztownia@gmail.com>
2022-10-19 00:56:33 +0300

    Усовершенствован сценарий field.sh.

--- scripts/compile/tests/field.sh
+++ scripts/compile/tests/field.sh
@@ -2,13 +2,15 @@
 
 echo -e "\n[Compiling tests for field...]\n"
 
+dir=/modules/field/tests
+
 fpc -vewn -Fu"./modules/field/;./modules/matrix/;./modules/test/" \
-  ./modules/field/tests/delete_filled_lines.pas && echo -e ""
+  .$dir/delete_filled_lines.pas && echo -e ""
 fpc -vewn -Fu"./modules/field/;./modules/matrix/;./modules/test/" \
-  ./modules/field/tests/delete_line.pas && echo -e ""
+  .$dir/delete_line.pas && echo -e ""
 fpc -vewn -Fu"./modules/field/;./modules/matrix/;./modules/test/" \
-  ./modules/field/tests/initialize.pas && echo -e ""
+  .$dir/initialize.pas && echo -e ""
 fpc -vewn -Fu"./modules/field/;./modules/matrix/;./modules/test/" \
-  ./modules/field/tests/is_filled.pas && echo -e ""
+  .$dir/is_filled.pas && echo -e ""
 fpc -vewn -Fu"./modules/field/;./modules/matrix/;./modules/test/" \
-  ./modules/field/tests/is_line_filled.pas && echo -e ""
+  .$dir/is_line_filled.pas && echo -e ""
