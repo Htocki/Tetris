d61ce241f8485f2a74131303f0b60b905f8dbb1c
Htocki <pasztownia@gmail.com>
2022-10-19 00:17:14 +0300

    Усовершествован сценарий compile.sh.

--- compile.sh
+++ compile.sh
@@ -1,30 +1,34 @@
 #!/bin/bash
 
 # Удаление мусорных файлов.
-sh scripts/remove/console.sh
-sh scripts/remove/element.sh
-sh scripts/remove/field.sh
-sh scripts/remove/figure.sh
-sh scripts/remove/key.sh
-sh scripts/remove/keyboard.sh
-sh scripts/remove/matrix.sh
-sh scripts/remove/model.sh
-sh scripts/remove/state.sh
-sh scripts/remove/target.sh
-sh scripts/remove/test.sh
-sh scripts/remove/user.sh
+dir=scripts/remove
+sh $dir/console.sh
+sh $dir/element.sh
+sh $dir/field.sh
+sh $dir/figure.sh
+sh $dir/key.sh
+sh $dir/keyboard.sh
+sh $dir/matrix.sh
+sh $dir/model.sh
+sh $dir/state.sh
+sh $dir/target.sh
+sh $dir/test.sh
+sh $dir/user.sh
 
 # Компиляция тестов.
-sh scripts/compile/tests/field.sh
-sh scripts/compile/tests/figure.sh
-sh scripts/compile/tests/matrix.sh
-sh scripts/compile/tests/target.sh
+dir=scripts/compile/tests
+sh $dir/field.sh
+sh $dir/figure.sh
+sh $dir/matrix.sh
+sh $dir/target.sh
 
 # Запуск тестов.
-sh scripts/run/field.sh
-sh scripts/run/figure.sh
-sh scripts/run/matrix.sh
-sh scripts/run/target.sh
+dir=scripts/run
+sh $dir/field.sh
+sh $dir/figure.sh
+sh $dir/matrix.sh
+sh $dir/target.sh
 
 # Компиляция тетриса.
-sh scripts/compile/tetris.sh
+dir=scripts/compile
+sh $dir/tetris.sh
