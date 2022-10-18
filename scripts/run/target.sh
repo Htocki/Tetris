ed5771431deb14f58ebd7d5f5d39c352bf5fa37f
Htocki <pasztownia@gmail.com>
2022-10-19 00:26:57 +0300

    Усовершенствован сценарий target.sh.

--- scripts/run/target.sh
+++ scripts/run/target.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo -e "\n[Running tests for target...]\n"
-
-./modules/target/tests/delete
-./modules/target/tests/initialize
+dir=/modules/target/tests
+.$dir/delete
+.$dir/initialize
