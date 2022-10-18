9b7b341684ddfc8ea27d957b375b805e8b54feb6
Htocki <pasztownia@gmail.com>
2022-10-19 00:18:40 +0300

    Усовершенствован сценарий field.sh.

--- scripts/run/field.sh
+++ scripts/run/field.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 echo -e "\n[Running tests for field...]\n"
-./modules/field/tests/delete_filled_lines
-./modules/field/tests/delete_line
-./modules/field/tests/initialize
-./modules/field/tests/is_filled
-./modules/field/tests/is_line_filled
+dir=/modules/field/tests
+.$dir/delete_filled_lines
+.$dir/delete_line
+.$dir/initialize
+.$dir/is_filled
+.$dir/is_line_filled
