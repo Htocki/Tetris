5a421cffc1d2865e5090d23015ab9db988471290
Htocki <pasztownia@gmail.com>
2022-10-19 01:06:59 +0300

    Усовершенствован сценарий target.sh.

--- scripts/compile/tests/target.sh
+++ scripts/compile/tests/target.sh
@@ -2,9 +2,11 @@
 
 echo -e "\n[Compiling tests for target...]\n"
 
+dir=/modules/target/tests
+
 fpc -vewn -Fu"./modules/element/;./modules/figure/;./modules/target/; \
-  ./modules/test/;./modules/matrix/" ./modules/target/tests/delete.pas && \
+  ./modules/test/;./modules/matrix/" .$dir/delete.pas && \
   echo -e ""
 fpc -vewn -Fu"./modules/element/;./modules/figure/;./modules/target/; \
-  ./modules/test/;./modules/matrix/" ./modules/target/tests/initialize.pas && \
+  ./modules/test/;./modules/matrix/" .$dir/initialize.pas && \
   echo -e ""
